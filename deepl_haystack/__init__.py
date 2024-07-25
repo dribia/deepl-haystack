34782ad882853fcbc685ee8784ee84d38fe7ae44
Albert Iribarne <iribarne@dribia.com>
2024-07-25 20:45:31 +0200

    :bookmark: Version patch to v0.1.1

--- deepl_haystack/__init__.py
+++ deepl_haystack/__init__.py
@@ -5,6 +5,6 @@ from .components.translators.deepl.translator import (
     DeepLTextTranslator,
 )
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["DeepLDocumentTranslator", "DeepLTextTranslator"]
