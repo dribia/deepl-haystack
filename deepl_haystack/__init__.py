22cd3d89615828f96c725b7d691cfd22e119e86f
Albert Iribarne <iribarne@dribia.com>
2024-07-26 16:44:38 +0200

    :sparkles: Add dynamic version depending on the installed version of the package

--- deepl_haystack/__init__.py
+++ deepl_haystack/__init__.py
@@ -1,10 +1,15 @@
 """DeepL Haystack integration."""
 
+from importlib.metadata import PackageNotFoundError, version
+
 from .components.translators.deepl.translator import (
     DeepLDocumentTranslator,
     DeepLTextTranslator,
 )
 
-__version__ = "0.1.1"
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
 
 __all__ = ["DeepLDocumentTranslator", "DeepLTextTranslator"]
